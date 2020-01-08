b59bac1f3ca1e3f56e5315dad10dc515f7757dab
jonassibbesen <j.a.sibbesen@gmail.com>
2020-01-08 13:57:00 -0800

    init calc_rsem_fragment_length_stats.R

---
+++ calc_rsem_fragment_length_stats.R
@@ -0,0 +1,23 @@
+
+# calc_rsem_fragment_length_stats.R
+# Calculates fragment length mean and standard deviation 
+# from RSEM model file.
+
+args <- commandArgs()
+script_dir <- dirname(sub("--file=", "", args[4]))
+print(script_dir)
+
+print(args)
+system(paste(c("git", "-C", script_dir, "rev-parse", "HEAD"), collapse = " "))
+system(paste(c("git", "-C", script_dir, "rev-parse", "--abbrev-ref", "HEAD"), collapse = " "))
+
+model_data <- read.csv(args[6], sep = " ", header = F)
+
+bounds <-  model_data[3,seq(1,3)]
+values <- sample(seq(bounds$V1 + 1, bounds$V2), size = 1000000, replace = T, prob =t(model_data[4,]))
+
+print("")
+print(paste("Mean:", mean(values)))
+print(paste("Sd:", sd(values)))
+
+print("Done")
